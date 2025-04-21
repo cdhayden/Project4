c9a125b590a5d4e62584c44803c1db7f236d199e
cdhayden <coledhayden@gmail.com>
2025-04-21 18:38:10 -0500

    updated scripts

--- 3way-pthread/program_threads.sh
+++ 3way-pthread/program_threads.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-THREADS=$(SLURM_CPUS_PER_TASK:-1)
+THREADS=${SLURM_CPUS_PER_TASK:-1}
+JOB=${SLURM_JOB_NAME}
 
-./pthreads ~dan/625/wiki_dump.txt $THREADS
-./pthreads ~dan/625/wiki_dump.txt $THREADS
-./pthreads ~dan/625/wiki_dump.txt $THREADS
+# Record the start time
+start_time=$(date +%s)
+
+# Run the program and collect resource stats
+/usr/bin/time -v ./pthreads ~dan/625/wiki_dump.txt $THREADS > program_output.txt 2> ./stats/time_stats_"$JOB"_"$1".txt
+##/usr/bin/time -v perf stat -d -o ./stats/time_stats_"$JOB"_"$1".txt ./pthreads ~dan/625/wiki_dump.txt $THREADS
+
+perf stat -d ./pthreads ~dan/625/wiki_dump.txt $THREADS > program_output.txt 2>> ./stats/time_stats_"$JOB"_"$1".txt
+##/usr/bin/time -v perf stat -d ./pthreads ~dan/625/wiki_dump.txt $THREADS > ./stats/time_stats_"$JOB"_"$1".txt 2>> ./stats/time_stats_"$JOB"_"$1".txt 1> ./program_output.txt
+
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate total elapsed time
+total_time=$((end_time - start_time))
+echo "Total Time: $total_time seconds" >> ./stats/time_stats_"$JOB"_"$1".txt
--- 3way-pthread/sbatch_script.sh
+++ 3way-pthread/sbatch_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sbatch -time=30 --mem=7G --cpus-per-task=1 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='1c' ./program_thread.sh
+for i in 1 2 3 4 5
+do
+##sbatch --time=30 --mem=7G --cpus-per-task=1 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='1c' program_threads.sh $i
+##sbatch --time=30 --mem=7G --cpus-per-task=2 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='2c' program_threads.sh $i
+##sbatch --time=20 --mem=7G --cpus-per-task=4 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='4c' program_threads.sh $i
+sbatch --time=10 --mem=7G --cpus-per-task=8 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='8c' program_threads.sh $i
+##sbatch --time=5 --mem=7G --cpus-per-task=16 --threads-per-core=1 --ntasks=1 --nodes=1 --constraint=moles --job-name='16c' program_threads.sh $i
+##sbatch --time=5 --mem=7G --cpus-per-task=8 --threads-per-core=1 --ntasks=2 --nodes=2 --constraint=moles --job-name='16c-2sys' program_threads.sh $i
+done
 
