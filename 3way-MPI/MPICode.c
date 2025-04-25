d8a624ad6416185b9a6e041c342efb24e8da2635
Battlord4Life <111781678+Battlord4Life@users.noreply.github.com>
2025-04-25 10:00:28 -0500

    Update MPICode.c using new pthread codes

--- 3way-MPI/MPICode.c
+++ 3way-MPI/MPICode.c
@@ -12,8 +12,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define MAX_LINE_LENGTH 8192  //Complete guess right now
-#define MAX_THREADS 1  // to change with core count
+int MAX_THREADS = 1;  // to change with core count
+int MAX_INPUT = 1; // vary input size
 
 // Structure to pass data to each thread
 typedef struct {
@@ -31,33 +31,33 @@ void *find_max_ascii(void *arg) {
     long size = data->end_offset - data->start_offset;
 
     int line_num = data->line_start;
-    char line[MAX_LINE_LENGTH];
-    long i = 0, line_idx = 0;
+    long i = 0;
+
+    int max_val = 0;
 
     while (i < size) {
         char c = buffer[i++];
         if (c == '\n' || i == size) {
-            line[line_idx] = '\0';
-            int max_val = 0;
-            for (int j = 0; line[j] != '\0'; j++) {
-                if ((unsigned char)line[j] > max_val) {
-                    max_val = (unsigned char)line[j];
-                }
-            }
             data->results[line_num++] = max_val;
-            line_idx = 0;
-        } else if (line_idx < MAX_LINE_LENGTH - 1) {
-            line[line_idx++] = c;
+            max_val = 0;
+        } else {
+            if ((unsigned char)c > max_val) {
+                max_val = (unsigned char)c;
+            }
         }
     }
+
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <file_path> <thread_count> <input_size>\n", argv[0]);
         return EXIT_FAILURE;
     }
+
+    sscanf(argv[2], "%d", &MAX_THREADS);
+    sscanf(argv[3], "%d", &MAX_INPUT);
 
     int fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
@@ -72,8 +72,6 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    
-
     long file_size = sb.st_size;
     char *file_data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (file_data == MAP_FAILED) {
@@ -82,27 +80,50 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    
+    // size check
+    long valid_size = 0;
+    int line_count = 0;
+    while (valid_size < file_size && line_count < MAX_LINES) {
+        if (file_data[valid_size] == '\n') {
+            line_count++;
+        }
+        valid_size++;
+    }
 
+    if (line_count < MAX_LINES) {
+        fprintf(stderr, "Warning: File only has %d lines. Proceeding with fewer lines.\n", line_count);
+        MAX_LINES = line_count;
+    }
+
+    int lines_per_thread = MAX_LINES / MAX_THREADS;
+    int remainder = MAX_LINES % MAX_THREADS;
+    
     int num_threads = MAX_THREADS;
     long chunk_size = file_size / num_threads;
     pthread_t threads[MAX_THREADS];
     ThreadData thread_data[MAX_THREADS];
     pthread_attr_t attr;
-    int *results = calloc(1000000, sizeof(int)); // Assuming 1M lines max
-
+    int *results = calloc(MAX_LINES, sizeof(int)); // depending on input size
 
 
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //Set Pthreads to joinable
 
     int current_line = 0;
+    long start = 0;
 
     for (int i = 0; i < num_threads; i++) {
-        long start = i * chunk_size;
+        int lines_for_thread = lines_per_thread + (i < remainder ? 1 : 0);
+        
+        start = i * chunk_size;
         long end = (i == num_threads - 1) ? file_size : (i + 1) * chunk_size;
 
-        // Align to next newline to prevent splitting a line
+        long local_start = start;
+        long local_end = local_start;
+        int lines = 0;
+
+        // Align to next newline to prevent splitting a line 
+        
         if (i != 0) {
             while (start < file_size && file_data[start] != '\n') start++;
             start++;
@@ -111,17 +132,26 @@ int main(int argc, char *argv[]) {
             while (end < file_size && file_data[end] != '\n') end++;
             end++;
         }
+        
+        
+        while (local_end < valid_size && lines < lines_for_thread) {
+            if (file_data[local_end] == '\n') {
+                lines++;
+            }
+            local_end++;
+        }
 
         thread_data[i].thread_id = i;
         thread_data[i].data = file_data;
-        thread_data[i].start_offset = start;
-        thread_data[i].end_offset = end;
+        thread_data[i].start_offset = local_start;
+        thread_data[i].end_offset = local_end;
         thread_data[i].line_start = current_line;
         thread_data[i].results = results;
 
         pthread_create(&threads[i], &attr, find_max_ascii, &thread_data[i]);
 
-        current_line += 1000000 / num_threads;
+        start = local_end;
+        current_line += lines_for_thread;
     }
 
     pthread_attr_destroy(&attr); //Free the atribute of the threads.
@@ -130,7 +160,7 @@ int main(int argc, char *argv[]) {
         pthread_join(threads[i], NULL);
     }
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < MAX_LINES; i++) {
         if (results[i] != 0) {
             printf("%d: %d\n", i, results[i]);
         }
