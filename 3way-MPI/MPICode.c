56b1ef7e5a69aeb4cf4dc8451c46dbedd96bee21
Battlord4Life <111781678+Battlord4Life@users.noreply.github.com>
2025-04-25 13:57:13 -0500

    Update MPICode.c for the master process

--- 3way-MPI/MPICode.c
+++ 3way-MPI/MPICode.c
@@ -14,6 +14,7 @@
 
 int MAX_THREADS = 1;  // to change with core count
 int MAX_INPUT = 1; // vary input size
+int MAX_LINES = 1; // vary amount of Lines total
 
 // Structure to pass data to each thread
 typedef struct {
@@ -25,7 +26,7 @@ typedef struct {
     int *results;
 } ThreadData;
 
-void *find_max_ascii(void *arg) {
+void *find_max_ascii(void *src, void *dest) {
     ThreadData *data = (ThreadData *)arg;
     char *buffer = data->data + data->start_offset;
     long size = data->end_offset - data->start_offset;
@@ -51,124 +52,173 @@ void *find_max_ascii(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <file_path> <thread_count> <input_size>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
 
-    sscanf(argv[2], "%d", &MAX_THREADS);
-    sscanf(argv[3], "%d", &MAX_INPUT);
-
-    int fd = open(argv[1], O_RDONLY);
-    if (fd < 0) {
-        perror("Failed to open file");
-        return EXIT_FAILURE;
-    }
-
-    struct stat sb;
-    if (fstat(fd, &sb) == -1) {
-        perror("fstat");
-        close(fd);
-        return EXIT_FAILURE;
-    }
-
-    long file_size = sb.st_size;
-    char *file_data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (file_data == MAP_FAILED) {
-        perror("mmap");
-        close(fd);
-        return EXIT_FAILURE;
-    }
-
-    // size check
-    long valid_size = 0;
-    int line_count = 0;
-    while (valid_size < file_size && line_count < MAX_LINES) {
-        if (file_data[valid_size] == '\n') {
-            line_count++;
-        }
-        valid_size++;
-    }
-
-    if (line_count < MAX_LINES) {
-        fprintf(stderr, "Warning: File only has %d lines. Proceeding with fewer lines.\n", line_count);
-        MAX_LINES = line_count;
-    }
-
-    int lines_per_thread = MAX_LINES / MAX_THREADS;
-    int remainder = MAX_LINES % MAX_THREADS;
     
-    int num_threads = MAX_THREADS;
-    long chunk_size = file_size / num_threads;
-    pthread_t threads[MAX_THREADS];
-    ThreadData thread_data[MAX_THREADS];
-    pthread_attr_t attr;
-    int *results = calloc(MAX_LINES, sizeof(int)); // depending on input size
+    
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <file_path> <thread_count> <input_size> <max_lines>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
+// Initialize the MPI environment
+	MPI_Init(&argc, &argv);
 
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //Set Pthreads to joinable
+    // Get the rank of the process
+	int pid;
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-    int current_line = 0;
-    long start = 0;
+	// Get the number of processes
+	int number_of_processes;
+	MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 
-    for (int i = 0; i < num_threads; i++) {
-        int lines_for_thread = lines_per_thread + (i < remainder ? 1 : 0);
-        
-        start = i * chunk_size;
-        long end = (i == num_threads - 1) ? file_size : (i + 1) * chunk_size;
+    if(pid == 0){
 
-        long local_start = start;
-        long local_end = local_start;
-        int lines = 0;
-
-        // Align to next newline to prevent splitting a line 
-        
-        if (i != 0) {
-            while (start < file_size && file_data[start] != '\n') start++;
-            start++;
+        sscanf(argv[2], "%d", &MAX_THREADS); //Read into the arguments
+        sscanf(argv[3], "%d", &MAX_INPUT);
+        sscanf(argv[4], "%d", &MAX_LINES);
+    
+        int fd = open(argv[1], O_RDONLY); //Open the file to read
+        if (fd < 0) {
+            perror("Failed to open file");
+            return EXIT_FAILURE;
         }
-        if (i != num_threads - 1) {
-            while (end < file_size && file_data[end] != '\n') end++;
-            end++;
+
+        struct stat sb; //Get the stats of the file for filesize
+        if (fstat(fd, &sb) == -1) {
+            perror("fstat");
+            close(fd);
+            return EXIT_FAILURE;
         }
+
+        int *results = calloc(MAX_LINES, sizeof(int)); // depending on Max lines
+        int resline = 0; //Line of the results buffer
         
+        if(sb.file_size/(NUM_THREADS-1) <= MAX_INPUT){ //If the file is smaller than expected input is larger than expected
+
+            int chunk = sb.file_size/(NUM_THREADS-1); //Find how much to read per input
+            int remainer = sb.file_size%(NUM_THREADS-1); //Find the remainer for the last thread to complete
+            int *buffer = calloc(chunk+1, sizeof(char)); // depending on input size
+
+            bool hold = false; //Hold varible for later in the for loops to check for non terminating lines
         
-        while (local_end < valid_size && lines < lines_for_thread) {
-            if (file_data[local_end] == '\n') {
-                lines++;
+            int piter = 1; //Iterator Varible for the processes
+            while (piter < MAX_THREADS-1){
+                read(fd, results, chunk);
+                buffer[chunk] = '\0'; //Set the last char to a null terminator for string reading
+                MPI_Send(buffer, chunk+1, MPI_CHAR, piter, 0, MPI_COMM_WORLD); //Send the buffer read to another branch
+                piter++; //Increment the process counter
             }
-            local_end++;
+            read(fd, results, remainer);
+            buffer[remainer] = '\0'; //Set the last char to a null terminator for string reading
+            MPI_Send(buffer, remainer, MPI_CHAR, piter, 0, MPI_COMM_WORLD); //Send the buffer read to another branch
+            
+            for(int i = 1; i < MAX_THREADS; i++){
+                int size = 0;
+                MPI_Recv(&size, 1, MPI_INT, i, 0,  MPI_COMM_WORLD);
+                int *cast = calloc(size, sizeof(int)); // depending on Max lines
+                MPI_Recv(cast, size, MPI_INT, i, 0, MPI_COMM_WORLD);
+                    
+                if(hold){//If hold is true
+                    if(results[resline-1] < cast[2]) { //If the incomplete line's value is less than the rest of the line, update it
+                
+                            results[resline-1] = cast[2];        
+                    }
+                    for(int j = 3; j < size; j++){
+
+                        results[resline] = cast[j];
+                        resline++;
+                    }
+                    hold = false;
+                }
+                else{
+                    for(int j = 2; j < size; j++){
+
+                        results[resline] = cast[j];
+                        resline++;   
+                    }
+                }
+                if(cast[1] == 1) //If the line doesnt terminate, make a note for the next iteration
+                {
+                    hold = true;
+                }
+                free(cast);
+            }
+            free(buffer);
         }
+        else{
+            int *buffer = calloc(MAX_INPUT, sizeof(char)); // depending on input size
+            bool done = false; //Bool to check if method is done
+            bool hold = false; //Hold varible for later in the for loops to check for non terminating lines
+            while(!done){ //While not done
+        
+                int piter = 1; //Iterator Varible for the processes
+                while (piter < MAX_THREADS && read(fd, results, MAX_INPUT - 1) != 0){
 
-        thread_data[i].thread_id = i;
-        thread_data[i].data = file_data;
-        thread_data[i].start_offset = local_start;
-        thread_data[i].end_offset = local_end;
-        thread_data[i].line_start = current_line;
-        thread_data[i].results = results;
+                    buffer[MAX_INPUT-1] = '\0'; //Set the last char to a null terminator for string reading
+                    MPI_Send(buffer, MAX_INPUT, MPI_CHAR, piter, 0, MPI_COMM_WORLD); //Send the buffer read to another branch
+                    piter++; //Increment the process counter
+                }
+                if(piter < MAX_THREADS) {//If the read got zero, (If both while conditons above occur, it'll loop for no downside)
+                    done = true;
+                } 
+                for(int i = 1; i < piter; i++){
 
-        pthread_create(&threads[i], &attr, find_max_ascii, &thread_data[i]);
+                    int size = 0;
+                    MPI_Recv(&size, 1, MPI_INT, i, 0,  MPI_COMM_WORLD);
+                    int *cast = calloc(size, sizeof(int)); // depending on Max lines
+                    MPI_Recv(cast, size, MPI_INT, i, 0, MPI_COMM_WORLD);
 
-        start = local_end;
-        current_line += lines_for_thread;
-    }
+                    if(hold){//If hold is true
+                        if(results[resline-1] < cast[2]) { //If the incomplete line's value is less than the rest of the line, update it
+                            results[resline-1] = cast[2];
+                        }
+                        for(int j = 3; j < size; j++){
 
-    pthread_attr_destroy(&attr); //Free the atribute of the threads.
+                            results[resline] = cast[j];
+                            resline++;
+                        }
+                        hold = false;
+                    }
+                    else{
+                        for(int j = 2; j < size; j++){
 
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    for (int i = 0; i < MAX_LINES; i++) {
-        if (results[i] != 0) {
-            printf("%d: %d\n", i, results[i]);
+                            results[resline] = cast[j];
+                            resline++;
+                        }
+                    }
+                    if(cast[1] == 1) //If the line doesnt terminate, make a note for the next iteration
+                    {
+                        hold = true;
+                    }
+                    free(cast);
+                }
+            }
+            free(buffer);
         }
+        for (int i = 0; i < MAX_LINES; i++) {
+            if (results[i] != 0) {
+                printf("%d: %d\n", i, results[i]);
+            }
+        }
+        free(results);
+        
+    }
+    else{
+
+        sscanf(argv[2], "%d", &MAX_THREADS); //Read into the arguments
+        sscanf(argv[3], "%d", &MAX_INPUT);
+        sscanf(argv[4], "%d", &MAX_LINES);
+
+        
+
     }
 
-    munmap(file_data, file_size);
+
+
+    
     close(fd);
-    free(results);
+    
+    MPI_Finalize();
 
     return EXIT_SUCCESS;
 }
